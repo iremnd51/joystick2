d8b0043c71cb6c6e122c2a5f51842f5862f3fea2
idemirnd <110859857+idemirnd@users.noreply.github.com>
2023-08-17 11:38:52 +0300

    Proje dosyası ekle.

---
+++ joystick2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace joystick2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(162, 176);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(113, 95);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += PanelJoystickMouseDown;
+            panel1.MouseMove += PanelJoystickMouseMove;
+            panel1.MouseUp += PanelJoystickMouseUp;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(491, 485);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+    }
+}
---
+++ joystick2/Form1.cs
@@ -0,0 +1,30 @@
+namespace joystick2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelJoystickMouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PanelJoystickMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PanelJoystickMouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
